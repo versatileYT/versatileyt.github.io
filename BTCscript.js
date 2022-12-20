13dc03dc751a35f8bfa4a0b0c596edf63db24459
versatileYT <106322999+versatileYT@users.noreply.github.com>
2022-12-20 18:34:41 +0300

    Add files via upload

---
+++ BTCscript.js
@@ -0,0 +1,23 @@
+let btc = 0;
+let vipNumber = 100;
+let = dollar;
+
+function a(){
+    btc++
+  dollar = (btc * 0.1)
+};
+function b(){
+    alert(`BTC: ${btc} = ${dollar}$`)
+}
+function VIP(){
+    window.location.href = 'file:///D:/JS/BTCvip.html';
+    alert("-100$")
+}
+function aVip(){
+    btc = btc + vipNumber;
+    dollar = (btc * 0.1)
+};  
+function OutPut(){
+    alert("Fatal Error")
+    window.location.href = 'https://www.binance.com/ru';
+}
